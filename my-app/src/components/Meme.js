5804749b51d2cc3253d8efc3225d491f025418e9
mariessad <74148632+mariessad@users.noreply.github.com>
2022-07-25 14:55:48 -0400

    change form to div

--- my-app/src/components/Meme.js
+++ my-app/src/components/Meme.js
@@ -1,21 +1,23 @@
 import React from "react";
+import memesData from "../memesData";
 
 export default function Meme() {
   return (
-    <form className="meme--form">
-      <input
-        type={"text"}
-        placeholder="top text"
-        className="meme--input"
-      ></input>
-      <input
-        type={"text"}
-        placeholder="bottom text"
-        className="meme--input"
-      ></input>
-      <button type={"submit"} className="meme--submit">
-        Get a new Meme image
-      </button>
-    </form>
+    <main>
+      <div className="meme--form">
+        <input
+          type={"text"}
+          placeholder="top text"
+          className="meme--input"
+        ></input>
+        <input
+          type={"text"}
+          placeholder="bottom text"
+          className="meme--input"
+        ></input>
+        <button className="meme--submit">Get a new Meme image</button>
+      </div>
+      {/* <img src={memesData.url}></img> */}
+    </main>
   );
 }
